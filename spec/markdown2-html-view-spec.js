4283deb94b995056892379f5bc40789629d97883
yaozh16 <yaozh16@qq.com>
2018-01-29 02:38:26 +0800

    first commit

---
+++ spec/markdown2-html-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import Markdown2HtmlView from '../lib/markdown2-html-view';
+
+describe('Markdown2HtmlView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
